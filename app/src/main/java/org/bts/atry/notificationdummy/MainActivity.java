9736111b9c24d82221da0d5d3008e175b8a49c70
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-02-20 09:39:22 +0100

    Initial commit

---
+++ app/src/main/java/org/bts/atry/notificationdummy/MainActivity.java
@@ -0,0 +1,13 @@
+package org.bts.atry.notificationdummy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
