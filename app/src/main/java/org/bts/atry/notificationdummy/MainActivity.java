6c133d44ea7dcc88304264361767f54848fb0ad7
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-02-20 10:13:49 +0100

    Set onclick Listerner Buttons Main

--- app/src/main/java/org/bts/atry/notificationdummy/MainActivity.java
+++ app/src/main/java/org/bts/atry/notificationdummy/MainActivity.java
@@ -2,12 +2,40 @@ package org.bts.atry.notificationdummy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnStandardNotification = (Button) findViewById(R.id.buttonToNotify);
+        final Button btnCustomNotification = (Button) findViewById(R.id.buttonToCustomNotify);
+        btnStandardNotification.setOnClickListener(this);
+        btnCustomNotification.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonToNotify:
+                Log.i(MainActivity.TAG, "Notify btn clicked");
+                break;
+            case R.id.buttonToCustomNotify:
+                Log.i(MainActivity.TAG, "Custom Notify btn clicked");
+                break;
+            default:
+                Log.i(MainActivity.TAG, "btn not linked");
+                break;
+        }
     }
 }
